a02634f8f193e29d89c13b990bd3ddfae1b369d5
YunBeomOh <dbsqja353@naver.com>
2023-06-19 15:27:25 +0900

    23_06_19 Still modifying
    API를 이용하여 cnt add/minus 수정 중

--- bustop_app/bustop_app/MainPage.xaml.cs
+++ bustop_app/bustop_app/MainPage.xaml.cs
@@ -4,6 +4,11 @@ using System.Data;
 using System.Diagnostics;
 using bustop_app.Logics;
 using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+using System.Net.Http.Json;
 
 namespace bustop_app;
 
@@ -18,14 +23,13 @@ public partial class MainPage : ContentPage
         MainViewModel vm = new MainViewModel();
 		BindingContext = vm;
 		Title = "";
-	}
+        
+    }
 
     private async void ContentPage_Loaded(object sender, EventArgs e)
     {
-        //RestAPI 기본 URI 호출
-        client.BaseAddress = new Uri("http://210.119.12.69:7058/"); // RestAPI 서버 기본 URL
-        //헤더 설정
-        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        client.BaseAddress = new Uri("http://210.119.12.69:7058/");//RestAPI 서버 기본 URL
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));//헤더설정
     }
 
     private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
@@ -34,44 +38,86 @@ public partial class MainPage : ContentPage
     }
 
     //LoadDB는 MainPage.xaml.cs 에서 사용하기 위한 Search함수
-    private void LoadDB(object sender, EventArgs e)
+    private async void LoadDB(object sender, EventArgs e)
     {
         MainViewModel vm = (MainViewModel)BindingContext;
         vm.Items.Clear();
-        using (MySqlConnection conn = new MySqlConnection(commons.myConnString))
+        try
         {
-            if (conn.State == ConnectionState.Closed)
-                conn.Open();
+            HttpResponseMessage? response = await client.GetAsync("api/BusTables");
+            response.EnsureSuccessStatusCode();
 
-            var query = @"SELECT * 
-                              FROM bus_table 
-                              ORDER BY bus_idx ASC";
-
-            //MySqlCommand command = new MySqlCommand(query, conn);
-            //MySqlDataReader reader = command.ExecuteReader();
-            var cmd = new MySqlCommand(query, conn);
-            var adapter = new MySqlDataAdapter(cmd);
-            var dSet = new DataSet();
-            adapter.Fill(dSet, "businfor");
-            foreach (DataRow dr in dSet.Tables["businfor"].Rows)
+            var busInfors = await response.Content.ReadAsAsync<Object>();
+            var result = Newtonsoft.Json.JsonConvert.SerializeObject(busInfors);
+            var jArray = JArray.Parse(result.ToString());
+            //var items = await response.Content.ReadAsAsync<ObservableCollection<businfor>>();
+            //busInfors.CopyForm(items);
+            foreach (var busInfo in jArray)
             {
                 vm.Items.Add(new businfor
                 {
-                    Bus_idx = Convert.ToInt32(dr["bus_idx"]),
-                    Bus_num = Convert.ToString(dr["bus_num"]) + "번",
-                    Bus_cnt = Convert.ToInt32(dr["bus_cnt"]) + "명",
-                    Bus_gap = Convert.ToInt32(dr["bus_gap"]) + "분",
-                    Bus_NowIn = Convert.ToInt32(dr["bus_NowIn"]) + "명"
+                    Bus_idx = Int32.Parse(busInfo["busIdx"].ToString()),
+                    Bus_num = busInfo["busNum"].ToString() + "번",
+                    Bus_cnt = busInfo["busCnt"].ToString() + "명",
+                    Bus_gap = busInfo["busGap"].ToString() + "분",
+                    Bus_NowIn = busInfo["busNowIn"].ToString() + "명"
+                    //Bus_num=$"{busInfo.Bus_num}번",
+                    //Bus_cnt=$"{busInfo.Bus_gap}번",
+                    //Bus_gap=$"{busInfo.Bus_gap}분",
+                    //Bus_NowIn=$"{busInfo.Bus_NowIn}명"
                 });
             }
-            conn.Close();
         }
+        catch (Newtonsoft.Json.JsonException jEx)
+        {
+            Console.WriteLine(jEx.Message);
+            //await(this.ShowMessageAsync("error", jEx.Message, MessageDialogStyle.Affirmative, new MetroDialogSettings()
+            //{ AnimateShow = true, AnimateHide = true }));
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine(ex.Message);
+            //await(this.ShowMessageAsync("error", ex.Message, MessageDialogStyle.Affirmative, new MetroDialogSettings()
+            //{ AnimateShow = true, AnimateHide = true }));
+        }
+
+        //MainViewModel vm = (MainViewModel)BindingContext;
+        //vm.Items.Clear();
+        //using (MySqlConnection conn = new MySqlConnection(commons.myConnString))
+        //{
+        //    if (conn.State == ConnectionState.Closed)
+        //        conn.Open();
+
+        //    var query = @"SELECT * 
+        //                      FROM bus_table 
+        //                      ORDER BY bus_idx ASC";
+
+        //    //MySqlCommand command = new MySqlCommand(query, conn);
+        //    //MySqlDataReader reader = command.ExecuteReader();
+        //    var cmd = new MySqlCommand(query, conn);
+        //    var adapter = new MySqlDataAdapter(cmd);
+        //    var dSet = new DataSet();
+        //    adapter.Fill(dSet, "businfor");
+        //    foreach (DataRow dr in dSet.Tables["businfor"].Rows)
+        //    {
+        //        vm.Items.Add(new businfor
+        //        {
+        //            Bus_idx = Convert.ToInt32(dr["bus_idx"]),
+        //            Bus_num = Convert.ToString(dr["bus_num"]) + "번",
+        //            Bus_cnt = Convert.ToInt32(dr["bus_cnt"]) + "명",
+        //            Bus_gap = Convert.ToInt32(dr["bus_gap"]) + "분",
+        //            Bus_NowIn = Convert.ToInt32(dr["bus_NowIn"]) + "명"
+        //        });
+        //    }
+        //    conn.Close();
+        //}
     }
 
     private async void Addcnt_Clicked(object sender, EventArgs e)
     {
         string busCntString = selectedItem.Bus_cnt.Replace("명", "");
         string busNowInString = selectedItem.Bus_NowIn.Replace("명", "");
+        
 
         int bus_count = int.Parse(busCntString) + int.Parse(busNowInString);
 
@@ -80,20 +126,55 @@ public partial class MainPage : ContentPage
             await DisplayAlert("Warning", "탑승 인원은 50명을 초과할 수 없습니다!","OK");
             return;
         }
-        using (MySqlConnection conn = new MySqlConnection(commons.myConnString))
+        //var strBus_idx = selectedItem.Bus_idx.ToString();
+        //var AddBusCnt = $"api/BusTables/{selectedItem.Bus_idx}";
+        try
         {
-            if (conn.State == ConnectionState.Closed) conn.Open();
-            var strBus_idx = selectedItem.Bus_idx.ToString();
-
-            var query = @"UPDATE bus_table SET bus_cnt = bus_cnt+1 WHERE bus_idx = @strBus_idx";
-
-            MySqlCommand cmd = new MySqlCommand(query, conn);
-            cmd.Parameters.AddWithValue("@strBus_idx", strBus_idx);
-            cmd.ExecuteNonQuery();
-            conn.Close();
+            var add_businfor = new businfor()
+            {
+                Bus_idx = selectedItem.Bus_idx,
+                Bus_num = selectedItem.Bus_num.ToString(),
+                Bus_cnt = (Int32.Parse(selectedItem.Bus_cnt.Replace("명", "")) + 1).ToString()+"명",
+                Bus_gap = selectedItem.Bus_gap.ToString(),
+                Bus_NowIn = selectedItem.Bus_NowIn.ToString()
+            };
+            //var content = new StringContent(JsonConvert.SerializeObject(add_businfor),Encoding.UTF8,"application/json");
+            var response = await client.PutAsJsonAsync($"api/BusTables/{selectedItem.Bus_idx}",add_businfor);
+            if(response.IsSuccessStatusCode)
+            {
+                await DisplayAlert("탑승 대기", $"{selectedItem.Bus_num} 버스 탑승 대기 완료!", "확인");
+                LoadDB(sender, e);
+            }
+            else
+            {
+                var errorResponse = await response.Content.ReadAsStringAsync();
+                await DisplayAlert("실패", $"{errorResponse}", "확인");
+            }
+            //await DisplayAlert("탑승 대기", $"{selectedItem.Bus_num} 버스 탑승 대기 완료!", "확인");
+            //LoadDB(sender, e);
         }
-        await DisplayAlert("탑승 대기", $"{selectedItem.Bus_num} 버스 탑승 대기 완료!", "확인");
-        LoadDB(sender,e);
+        catch (Newtonsoft.Json.JsonException jEx)
+        {
+            Console.WriteLine(jEx.Message);
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        //using (MySqlConnection conn = new MySqlConnection(commons.myConnString))
+        //{
+        //    if (conn.State == ConnectionState.Closed) conn.Open();
+        //    var strBus_idx = selectedItem.Bus_idx.ToString();
+
+        //    var query = @"UPDATE bus_table SET bus_cnt = bus_cnt+1 WHERE bus_idx = @strBus_idx";
+
+        //    MySqlCommand cmd = new MySqlCommand(query, conn);
+        //    cmd.Parameters.AddWithValue("@strBus_idx", strBus_idx);
+        //    cmd.ExecuteNonQuery();
+        //    conn.Close();
+        //}
+        //await DisplayAlert("탑승 대기", $"{selectedItem.Bus_num} 버스 탑승 대기 완료!", "확인");
+        //LoadDB(sender,e);
     }
 
     private async void Minuscnt_Clicked(object sender, EventArgs e)
